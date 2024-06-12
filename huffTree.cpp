1b1d8ea0bfee48465c78512a40eecea349454c61
Giuliano <giulianochiochetta@gmail.com>
2024-06-12 12:16:20 -0300

    arquivos_certos

--- huffTree.cpp
+++ huffTree.cpp
@@ -4,11 +4,13 @@
 #include <string>
 #include <vector> 
 #include <fstream>
+#include <codecvt>
+#include <locale>
 
 using namespace std;
 
 struct node{
-    char ch;
+    wchar_t ch;
     int freq;
     node *left;
     node *right;
@@ -23,7 +25,7 @@ struct comp {
 
 
 //funcao para alocar um novo nodo na memoria
-node *create_node(char c, int frequency, node *left, node *right){
+node *create_node(wchar_t c, int frequency, node *left, node *right){
     node *p = new node;
     p->ch = c;
     p->freq = frequency;
@@ -33,18 +35,18 @@ node *create_node(char c, int frequency, node *left, node *right){
     return p;
 }
 
-void encode(node *root, string str, unordered_map<char, string> &huffcode){
+void encode(node *root, wstring str, unordered_map<wchar_t, wstring> &huffcode){
     if(!root) return;
 
     if(!root->left && !root->right){
         huffcode[root->ch] = str;
     }
 
-    encode(root->left, str + "0", huffcode);
-    encode(root->right, str + "1", huffcode);
+    encode(root->left, str + L'0', huffcode);
+    encode(root->right, str + L'1', huffcode);
 }
 
-void decode(node *root, int &top_index, string str){
+void decode(node *root, int &top_index, wstring str){
     if(!root) return;
 
     if(!root->left && !root->right){
@@ -53,27 +55,32 @@ void decode(node *root, int &top_index, string str){
     }
     top_index++;
 
-    if(str[top_index] == '0') decode(root->left, top_index, str);
-    else if(str[top_index] == '1') decode(root->right, top_index, str);
+    if(str[top_index] == L'0') decode(root->left, top_index, str);
+    else if(str[top_index] == L'1') decode(root->right, top_index, str);
 }
 
 
-void buildHuffTree(string text){
+void buildHuffTree(wifstream &arq, wofstream &saida){
 
-    unordered_map<char, int> freq;
-    for(char ch : text){
+    if(!arq){
+        return;
+    }
+    
+    wchar_t ch;
+    unordered_map<wchar_t,  int> freq;
+    while(arq.get(ch)){
         freq[ch]++;
     }
 
     int teste = 0; //contador somente para fins de teste 
-    cout << "Frequencia de caracteres: \n";
+    saida << L"Frequência de caracteres: \n";
     for(auto ch : freq){ 
-        if(ch.first == ' ') cout << "SPACE: " << ch.second << endl;
-        else if(ch.first == '\n') cout << "\\n: " << ch.second << endl;
-        else cout << ch.first << ": " << ch.second << endl;
-        teste += ch.second; //esta inconsistente, ver pq
+        if(ch.first == L' ') saida << L"SPACE: " << ch.second << endl;
+        else if(ch.first == L'\n') saida << L"\\n: " << ch.second << endl;
+        else saida << ch.first << L": " << ch.second << endl;
+        teste += ch.second;
     }
-    cout << teste << endl << endl;
+    saida << teste << endl << endl;
 
     //priority_queue é uma estrutura muito similar a queue
     //no entanto ela sempre armazena os valores de tal modo
@@ -97,43 +104,42 @@ void buildHuffTree(string text){
         // e a frequencia sendo igual a soma das frequencias dos filhos.
         //E adiciona o novo nodo a fila de prioridade;  
         int soma =left->freq + right->freq;
-        pq.push(create_node('\0', soma, left, right));
+        pq.push(create_node(L'\0', soma, left, right));
     }
 
     node *root = pq.top();
-    unordered_map<char, string> huffCode;
-    encode(root, "", huffCode);
+    unordered_map<wchar_t, wstring> huffCode;
+    encode(root, L"", huffCode);
 
-    cout << "Codigos binarios da arvore criada: \n";
+    saida << L"Codigos binários da árvore criada: \n";
     for(auto pair : huffCode){
-        if(pair.first == ' ') cout << "SPACE: " << pair.second << endl;
-        else if(pair.first == '\n') cout << "\\n: " << pair.second << endl;
-        else cout << pair.first << ": " << pair.second << endl;
-        
+        if(pair.first == L' ') saida << L"SPACE: " << pair.second << endl;
+        else if(pair.first == L'\n') saida << L"\\n: " << pair.second << endl;
+        else saida << pair.first << L": " << pair.second << endl;
     }
-
-    cout << "\nString original: " << text << endl;
-
-    string str = "";
-    for(char ch : text){
-        str += huffCode[ch];
-    }
-    cout << "String criptografada: " << str << endl;
-
-    int top_index = -1;
-    cout << "\nString decriptografada: ";
-    while(top_index < (int)str.size() - 2){
-        decode(root, top_index, str);
-    }
-    cout << endl;
-
 }
  
 
 int main(){
-    string text = "Big Bob Bites Banana";
+    wifstream arq("../clarissa.txt");
+    arq.imbue(locale(arq.getloc(), new codecvt_utf8<wchar_t>));
 
-    buildHuffTree(text);
+    wofstream saida("../saida.txt");
+    saida.imbue(locale(saida.getloc(), new codecvt_utf8<wchar_t>));
+    
+    if(!arq.is_open()){
+        wcout << L"Erro em abrir o arquivo!\n";
+        return 1;
+    }
+
+    if(!saida.is_open()){
+        wcout << L"Erro em criar arquivo de saída!\n";
+        return 1;
+    }
+
+    buildHuffTree(arq, saida);
+    arq.close();
+    saida.close();
 
     return 0;
 }
